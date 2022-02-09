01b990f618663961f444a8ae3cff5369bfa217f1
whereislima <reach.lima@gmail.com>
2022-02-08 21:40:12 -0800

    first commit

---
+++ App.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = "https://course-api.com/react-tours-project";
+
+function App() {
+  const [loading, setLoading] = useState(false);
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
+  // anytime fetching data from web, need to use async and await, review from javascript
+  const getTours = async () => {
+    const response = await fetch(url);
+    const tours = await response.json();
+    // console.log(tours);
+    setTours(tours);
+  };
+  // call and execute function above, adding the dependency array [] will prevent re-rendering
+  useEffect(() => {
+    getTours();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className='title'>
+          <h2>no tours left</h2>
+          <button className='btn' onClick={getTours}>
+            refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
+
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  );
+}
+
+export default App;
